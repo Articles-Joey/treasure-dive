850db1f629d30cf70777407007a3e4a4dbdc4844
ArticlesJoey <joey@articles.media>
2025-12-17 15:05:53 -0500

    Package updates React compiler added

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    poweredByHeader: false,
+    reactCompiler: true,
     images: {
         // domains: ['cdn.articles.media', 'articles-website.s3.amazonaws.com', 'd3bzp9rk94ifwy.cloudfront.net'],
         remotePatterns: [
