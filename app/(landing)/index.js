533e591873404b29fec8c195e230c509e0d4f9bc
ArticlesJoey <joey@articles.media>
2026-05-21 03:06:37 -0400

    Now using PageTemplateLandingPage

--- app/(landing)/index.js
+++ app/(landing)/index.js
@@ -1,242 +1,70 @@
 "use client"
-import { useEffect, useContext, useState } from 'react';
-
-import Image from 'next/image'
 import Link from 'next/link'
 import dynamic from 'next/dynamic'
 
-import ArticlesButton from '@/components/UI/Button';
 import { useSocketStore } from '@/hooks/useSocketStore';
 import { useStore } from '@/hooks/useStore';
 
 import logo from '@/app/icon.png'
 
-import useUserDetails from '@articles-media/articles-dev-box/useUserDetails';
-import useUserToken from '@articles-media/articles-dev-box/useUserToken';
-import NicknameInput from '@articles-media/articles-dev-box/NicknameInput';
-import GameMenuPrimaryButtonGroup from '@articles-media/articles-dev-box/GameMenuPrimaryButtonGroup';
-import SessionButton from '@articles-media/articles-dev-box/SessionButton';
-// import RotatingMascot from '@/components/UI/RotatingMascot';
+import PageTemplateLandingPage from '@articles-media/articles-dev-box/PageTemplateLandingPage';
+
 const RotatingMascot = dynamic(() =>
     import('@/components/UI/RotatingMascot'),
-    { ssr: false }
-);
-const ReturnToLauncherButton = dynamic(() =>
-    import('@articles-media/articles-dev-box/ReturnToLauncherButton'),
-    { ssr: false }
-);
-const GameScoreboard = dynamic(() =>
-    import('@articles-media/articles-dev-box/GameScoreboard'),
-    { ssr: false }
-);
-const Ad = dynamic(() =>
-    import('@articles-media/articles-dev-box/Ad'),
     { ssr: false }
 );
 
 export default function LobbyPage() {
 
-    const {
-        socket,
-    } = useSocketStore(state => ({
-        socket: state.socket,
-    }));
-
-    const nickname = useStore(state => state.nickname)
-    const darkMode = useStore(state => state.darkMode)
-    const lobbyDetails = useStore(state => state.lobbyDetails)
-
-    const {
-        data: userToken,
-        error: userTokenError,
-        isLoading: userTokenLoading,
-        mutate: userTokenMutate
-    } = useUserToken(
-        process.env.NEXT_PUBLIC_GAME_PORT
-    );
-
-    const {
-        data: userDetails,
-        error: userDetailsError,
-        isLoading: userDetailsLoading,
-        mutate: userDetailsMutate
-    } = useUserDetails({
-        token: userToken
-    });
-
     return (
+        <>
+            <PageTemplateLandingPage
+                useSocketStore={useSocketStore}
+                useStore={useStore}
+                RotatingMascot={RotatingMascot}
+                Link={Link}
+                logoImage={logo.src}
+                // LandingBackgroundAnimation={
+                //     <LandingBackgroundAnimation />
+                // }
+                // CardBodyOverride={<>
 
-        <div className={`landing-page`}>
+                // </>}
+                // disableHero
+                // heroOverride={<>
+                // </>}
+                backgroundImage={`${process.env.NEXT_PUBLIC_CDN}games/Treasure Dive/treasure-dive-thumbnail.png`}
+                singlePlayerConfig={{
 
-            <div className='background-wrap'>
-                <Image
-                    src={`${process.env.NEXT_PUBLIC_CDN}games/Treasure Dive/treasure-dive-thumbnail.png`}
-                    alt=""
-                    fill
-                    style={{ objectFit: 'cover', objectPosition: 'center', filter: 'blur(10px)' }}
-                />
-            </div>
-
-            <div className="container d-flex flex-column-reverse flex-lg-row justify-content-center align-items-center py-3">
-
-                <div
-                    style={{ "width": "20rem" }}
-                >
-
-                    <div className="d-flex flex-column align-items-center mb-4">
-                        <img
-                            src={logo.src}
-                        >
-                        </img>
-                        <h1 className="mb-0 original-surfer-regular">Treasure Dive</h1>
-                    </div>
-
-                    <div
-                        className="card card-articles card-sm mb-3"
-                    >
-
-                        <div className='card-header d-flex align-items-center'>
-
-                            <NicknameInput
-                                useStore={useStore}
-                            />
-
-                        </div>
-
-                        <div className="card-body">
-
-                            <Link href={{
-                                pathname: `/play`
-                            }}>
-                                <ArticlesButton
-                                    className={`w-100 mb-3`}
-                                    small
-                                >
-                                    <i className="fas fa-play"></i>
-                                    Play Single Player
-                                </ArticlesButton>
-                            </Link>
-
-                            <div className="fw-bold mb-1 small text-center">
-                                {lobbyDetails?.online_player_count || 0} player{lobbyDetails?.online_player_count > 1 && 's'} in the lobby.
-                            </div>
-
-                            <div className="servers">
-
-                                {[1, 2].map(id => {
-
-                                    let lobbyLookup = lobbyDetails?.fourFrogsGlobalState?.games?.find(lobby =>
-                                        parseInt(lobby.server_id) == id
-                                    )
-
-                                    return (
-                                        <div key={id} className="server">
-
-                                            <div className='d-flex justify-content-between align-items-center w-100 mb-2'>
-                                                <div className="mb-0" style={{ fontSize: '0.9rem' }}><b>Server {id}</b></div>
-                                                <div className='mb-0'>{lobbyLookup?.players?.length || 0}/4</div>
-                                            </div>
-
-                                            <div className='d-flex justify-content-around w-100 mb-1'>
-                                                {[1, 2, 3, 4].map(player_count => {
-
-                                                    let playerLookup = false
-
-                                                    if (lobbyLookup?.players?.length >= player_count) playerLookup = true
-
-                                                    return (
-                                                        <div key={player_count} className="icon" style={{
-                                                            width: '20px',
-                                                            height: '20px',
-                                                            ...(playerLookup ? {
-                                                                backgroundColor: 'black',
-                                                            } : {
-                                                                backgroundColor: 'gray',
-                                                            }),
-                                                            border: '1px solid black'
-                                                        }}>
-
-                                                        </div>
-                                                    )
-                                                })}
-                                            </div>
-
-                                            <Link
-                                                className={``}
-                                                href={{
-                                                    pathname: `/play`,
-                                                    query: {
-                                                        server: id
-                                                    }
-                                                }}
-                                            >
-                                                <ArticlesButton
-                                                    className="px-5"
-                                                    small
-                                                >
-                                                    Join
-                                                </ArticlesButton>
-                                            </Link>
-
-                                        </div>
-                                    )
-                                })}
-
-                            </div>
-
-                        </div>
-
-                        <div className="card-footer d-flex flex-wrap justify-content-center">
-
-                            <GameMenuPrimaryButtonGroup
-                                useStore={useStore}
-                                type="Landing"
-                            />
-
-                        </div>
-
-                    </div>
-
-                    <SessionButton
-                        port={process.env.NEXT_PUBLIC_GAME_PORT}
-                        friendsButton={true}
-                    />
-
-                    <ReturnToLauncherButton />
-
-                </div>
-
-                <GameScoreboard
-                    game={process.env.NEXT_PUBLIC_GAME_NAME}
-                    style="Default"
-                    darkMode={darkMode ? true : false}
-                    prepend={
-                        <div
-                            style={{
-                                width: '100%',
-                                height: '200px',
-                                display: 'flex',
-                                justifyContent: 'center',
-                                alignItems: 'center',
-                            }}
-                        >
-                            <RotatingMascot />
-                        </div>
-                    }
-                />
-
-                <Ad
-                    style="Default"
-                    section={"Games"}
-                    section_id={process.env.NEXT_PUBLIC_GAME_NAME}
-                    darkMode={darkMode ? true : false}
-                    user_ad_token={userToken}
-                    userDetails={userDetails}
-                    userDetailsLoading={userDetailsLoading}
-                />
-
-            </div>
-
-        </div>
+                }}
+                NicknameInputConfig={{
+                    // PreComponent: <></>,
+                }}
+                multiplayerConfig={{
+                    type: "WebSocket",
+                    // comingSoon: true,
+                    defaultServers: 2,
+                    // privateServerSupport: false,
+                }}
+                gameScoreboardConfig={{
+                    append_score_text: "m",
+                    metrics: [
+                        {
+                            label: 'Treasures Collected',
+                            key: "score",
+                            format: (value) => `${value} m`
+                        },
+                        {
+                            label: 'Distance Swam',
+                            key: "total_distance",
+                            format: (value) => `${value} m`
+                        }
+                    ]
+                }}
+                brandingTextClass="original-surfer-regular"
+                disableGameScoreboard={process.env.NEXT_PUBLIC_ENABLE_ARTICLES !== 'true'}
+                disableAd={process.env.NEXT_PUBLIC_ENABLE_ARTICLES !== 'true'}
+            />
+        </>
     );
 }
